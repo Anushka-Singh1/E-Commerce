f790fe502a73f37f02191af341271c16b55108c8
Anushka-Singh1 <anushka.sng2002@gmail.com>
2024-06-25 14:10:34 +0530

    hamburger added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { SiPrestashop } from "react-icons/si";
 import { FaHome } from "react-icons/fa";
@@ -6,8 +6,12 @@ import { AiFillProduct } from "react-icons/ai";
 import { IoNewspaper } from "react-icons/io5";
 import { TbPhoneCalling } from "react-icons/tb";
 import { BsCart4 } from "react-icons/bs";
+import { PiHamburgerLight } from "react-icons/pi";
+import { MdOutlineClose } from "react-icons/md";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State to manage menu open/close
+
   const navItems = [
     { to: "/", icon: <FaHome className='h-6 w-8' />, label: "Home" },
     { to: "/Products", icon: <AiFillProduct className='h-6 w-8' />, label: "Products" },
@@ -25,7 +29,8 @@ function Navbar() {
         </NavLink>
       </div>
       
-      <div className='flex space-x-6 items-center'>
+      {/* Desktop Menu */}
+      <div className='hidden md:flex space-x-6 items-center'>
         {navItems.map((item, index) => (
           item.isButton ? (
             <NavLink to={item.to} key={index} className="text-white">
@@ -39,6 +44,39 @@ function Navbar() {
           )
         ))}
       </div>
+      
+      {/* Mobile Menu Toggle */}
+      <div className='md:hidden flex items-center z-[100001]'>
+        {/* Conditional rendering for Hamburger and Close icons */}
+        {isMenuOpen ? (
+          <MdOutlineClose 
+            className='h-8 w-8 text-white cursor-pointer z-[100002]' 
+            onClick={() => setIsMenuOpen(false)} // Close the menu
+          />
+        ) : (
+          <PiHamburgerLight 
+            className='h-8 w-8 text-white cursor-pointer' 
+            onClick={() => setIsMenuOpen(true)} // Open the menu
+          />
+        )}
+      </div>
+
+      {/* Mobile Menu */}
+      {isMenuOpen && (
+        <div className='absolute top-0 left-0 w-full h-screen bg-amber-300 flex flex-col items-center justify-center z-[100000]'>
+          {navItems.map((item, index) => (
+            <NavLink 
+              to={item.to} 
+              key={index} 
+              className="flex items-center space-x-2 text-white text-2xl mb-4" 
+              onClick={() => setIsMenuOpen(false)} // Close menu on link click
+            >
+              {item.icon}
+              <span>{item.label}</span>
+            </NavLink>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
