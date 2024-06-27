b03a6ccc71d95138a6312dce47d484f412ade2e9
Anushka-Singh1 <anushka.sng2002@gmail.com>
2024-06-27 19:52:27 +0530

    Error page added

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Image from '../assets/Error.jpg';
 
 function ErrorPage() {
   return (
-    <>
-        <h1>404</h1>
-        <h2>Page not found</h2>
-    </>
-  )
+    <div className="relative flex flex-col justify-center items-center h-screen w-full">
+  
+      <img 
+        src={Image} 
+        alt="error" 
+        className="absolute top-0 left-0 w-full h-full object-cover"
+      />
+
+   
+      <div className="relative flex flex-col justify-center items-center h-full w-full bg-black bg-opacity-50 p-4">
+        <div className="text-center font-bold text-white font-serif text-3xl md:text-5xl mb-4 md:mb-8">
+          Oops! Page not found
+        </div>
+        
+        <NavLink to="/">
+          <button className="bg-transparent hover:bg-fuchsia-800 text-fuchsia-800 font-semibold hover:text-white py-2 px-3 border border-fuchsia-900 hover:border-transparent rounded-lg">
+            Go Back
+          </button>
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
-export default ErrorPage
+export default ErrorPage;
