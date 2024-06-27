0160d5b2de83908ff8fe73e59cd584376a1703f5
Anushka-Singh1 <anushka.sng2002@gmail.com>
2024-06-27 17:09:22 +0530

    Added footer component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className=' bg-fuchsia-900 w-full h-8 mt-8 flex justify-center items-center text-center font-semibold font serif'>
+      &copy; 2021 E-Commerce. All rights reserved.
+    </div>
+  )
+}
+
+export default Footer
