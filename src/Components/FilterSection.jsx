7d3e315ccfd9835cdccf9ebdcfe692cf8ff87fb6
Anushka-Singh1 <anushka.sng2002@gmail.com>
2024-07-17 23:12:09 +0530

    Added search-filter-option in filtersection

--- src/Components/FilterSection.jsx
+++ src/Components/FilterSection.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { useFilterContext } from '../Context/FilterContext'
 
 function FilterSection() {
+  const {
+    filters: { text },
+    updateFilterValue
+  } = useFilterContext();
+
   return (
     <div>
-      
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          className='border-2 border-black focus:outline-none focus:border-fuchsia-800 focus:ring-0'
+          type="text"
+          name="text"
+          value={text}
+          onChange={updateFilterValue}
+        />
+      </form>
     </div>
-  )
+  );
 }
 
 export default FilterSection
--- src/Context/FilterContext.jsx
+++ src/Context/FilterContext.jsx
@@ -9,6 +9,9 @@ const initialState = {
   all_products: [],
   grid_view: true,
   sorting_value:"lowest",
+  filters:{
+    text: "",
+  },
 };
 
 const FilterContextProvider = ({ children }) => {
@@ -32,18 +35,25 @@ const FilterContextProvider = ({ children }) => {
 
   //sorting function useEffect
   useEffect(() => {
+    dispatch({type: "FILTER_PRODUCTS"});
     dispatch({ type: "SORTING_PRODUCTS" });
-  }, [products, state.sorting_value]);
-
+  }, [products, state.sorting_value, state.filters]);
+ 
 
   // TO load all the products for list and grid view
   useEffect(() => {
     dispatch({ type: "Load_Filter_Products", payload: products });
   }, [products]);
 
+  //update the filter values
+  const updateFilterValue =(event)=>{
+    let name = event.target.name;
+    let value = event.target.value;
+    return dispatch({type: "UPDATE_FILTER_VALUE", payload: {name, value}});
+  }
   
   return (
-    <FilterContext.Provider value={{ ...state , setGridView, setListView, sorting}}>
+    <FilterContext.Provider value={{ ...state , setGridView, setListView, sorting, updateFilterValue}}>
       {children}
     </FilterContext.Provider>
   );
